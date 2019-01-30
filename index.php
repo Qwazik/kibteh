8705c4a69b4efe0e6e9a0569658df9561a380b3b
Alexey <qwazik21@gmail.com>
2019-01-30 16:56:26 +0300

    Проверка

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("1С-Битрикс: Управление сайтом");
-?>
-
-Пустая страница. <a href="/bitrix/admin/">Перейти в Панель Управления</a>.
-
+$APPLICATION->SetTitle("1С-Битрикс: Управление сайтом");?>
+<?require($_SERVER["DOCUMENT_ROOT"].SITE_TEMPLATE_PATH."/home_content.php");?>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
